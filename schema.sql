204124cd74387e804725e8e03e4e00d2996e24b1
Samuel James <samuel.jlbsb@gmail.com>
2023-07-13 00:27:30 +0000

    Equema inicial do bd

---
+++ schema.sql
@@ -0,0 +1,267 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Tempo de geração: 12/07/2023 às 23:42
+-- Versão do servidor: 8.0.33
+-- Versão do PHP: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db_unb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `avaliacoes`
+--
+
+CREATE TABLE `avaliacoes` (
+  `id` int NOT NULL,
+  `usuario` int NOT NULL,
+  `id_turma` int NOT NULL,
+  `professor_nota` int NOT NULL,
+  `professor_text` text NOT NULL,
+  `disciplina_nota` int NOT NULL,
+  `disciplina_text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `denuncias`
+--
+
+CREATE TABLE `denuncias` (
+  `id` int NOT NULL,
+  `id_avaliacao` int NOT NULL,
+  `motivo` text NOT NULL,
+  `tratada` tinyint(1) NOT NULL,
+  `data` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `departamentos`
+--
+
+CREATE TABLE `departamentos` (
+  `cod` int NOT NULL,
+  `nome` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `disciplinas`
+--
+
+CREATE TABLE `disciplinas` (
+  `cod` varchar(16) NOT NULL,
+  `nome` varchar(512) NOT NULL,
+  `cod_depto` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `disciplina_professor`
+--
+
+CREATE TABLE `disciplina_professor` (
+  `cod_disciplina` varchar(16) NOT NULL,
+  `cod_professor` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `professores`
+--
+
+CREATE TABLE `professores` (
+  `matricula` int NOT NULL,
+  `cod_depto` int NOT NULL,
+  `nome` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `turmas`
+--
+
+CREATE TABLE `turmas` (
+  `id` int NOT NULL,
+  `periodo` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `cod_professor` int NOT NULL,
+  `cod_disciplina` varchar(16) NOT NULL,
+  `turma` int NOT NULL,
+  `horario` varchar(32) NOT NULL,
+  `local` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `matricula` int NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `curso` varchar(128) NOT NULL,
+  `nome` varchar(256) NOT NULL,
+  `senha` varchar(96) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`matricula`, `email`, `curso`, `nome`, `senha`, `is_admin`) VALUES
+(190019948, 'samuel.jlbsb@gmail.com', 'Engenharia de Computacao', 'Samuel James de Lima Barroso', '$not_a_password', 0);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `avaliacoes`
+--
+ALTER TABLE `avaliacoes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_turma` (`id_turma`),
+  ADD KEY `avaliacoes_ibfk_1` (`usuario`);
+
+--
+-- Índices de tabela `denuncias`
+--
+ALTER TABLE `denuncias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_avaliacao` (`id_avaliacao`);
+
+--
+-- Índices de tabela `departamentos`
+--
+ALTER TABLE `departamentos`
+  ADD PRIMARY KEY (`cod`);
+
+--
+-- Índices de tabela `disciplinas`
+--
+ALTER TABLE `disciplinas`
+  ADD PRIMARY KEY (`cod`),
+  ADD KEY `cod_depto` (`cod_depto`);
+
+--
+-- Índices de tabela `disciplina_professor`
+--
+ALTER TABLE `disciplina_professor`
+  ADD KEY `cod_professor` (`cod_professor`),
+  ADD KEY `cod_disciplina` (`cod_disciplina`);
+
+--
+-- Índices de tabela `professores`
+--
+ALTER TABLE `professores`
+  ADD PRIMARY KEY (`matricula`),
+  ADD KEY `cod_depto` (`cod_depto`);
+
+--
+-- Índices de tabela `turmas`
+--
+ALTER TABLE `turmas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `turma_unique` (`periodo`,`cod_disciplina`,`turma`) USING BTREE,
+  ADD KEY `cod_disciplina` (`cod_disciplina`),
+  ADD KEY `cod_professor` (`cod_professor`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`matricula`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `avaliacoes`
+--
+ALTER TABLE `avaliacoes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `denuncias`
+--
+ALTER TABLE `denuncias`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `turmas`
+--
+ALTER TABLE `turmas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `avaliacoes`
+--
+ALTER TABLE `avaliacoes`
+  ADD CONSTRAINT `avaliacoes_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`matricula`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `denuncias`
+--
+ALTER TABLE `denuncias`
+  ADD CONSTRAINT `denuncias_ibfk_1` FOREIGN KEY (`id_avaliacao`) REFERENCES `avaliacoes` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `disciplinas`
+--
+ALTER TABLE `disciplinas`
+  ADD CONSTRAINT `disciplinas_ibfk_1` FOREIGN KEY (`cod_depto`) REFERENCES `departamentos` (`cod`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `disciplina_professor`
+--
+ALTER TABLE `disciplina_professor`
+  ADD CONSTRAINT `disciplina_professor_ibfk_1` FOREIGN KEY (`cod_professor`) REFERENCES `professores` (`matricula`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `disciplina_professor_ibfk_2` FOREIGN KEY (`cod_disciplina`) REFERENCES `disciplinas` (`cod`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `professores`
+--
+ALTER TABLE `professores`
+  ADD CONSTRAINT `professores_ibfk_1` FOREIGN KEY (`cod_depto`) REFERENCES `departamentos` (`cod`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `turmas`
+--
+ALTER TABLE `turmas`
+  ADD CONSTRAINT `turmas_ibfk_1` FOREIGN KEY (`cod_disciplina`) REFERENCES `disciplinas` (`cod`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `turmas_ibfk_2` FOREIGN KEY (`cod_professor`) REFERENCES `professores` (`matricula`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
