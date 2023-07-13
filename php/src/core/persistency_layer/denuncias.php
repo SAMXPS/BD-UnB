f6f745af65b1e26a35ed35fe21ed89e794e1bbdf
Samuel James <samuel.jlbsb@gmail.com>
2023-07-13 00:28:02 +0000

    arquivos diversos php

---
+++ php/src/core/persistency_layer/denuncias.php
@@ -0,0 +1,18 @@
+<?php
+namespace denuncias;
+
+function create($data) {
+    // todo
+}
+
+function read($key) {
+    // todo
+}
+
+function update($key, $data) {
+    // todo
+}
+
+function delete($key) {
+    // todo
+}
---
+++ php/src/core/persistency_layer/departamentos.php
@@ -0,0 +1,18 @@
+<?php
+namespace departamentos;
+
+function create($data) {
+    // todo
+}
+
+function read($key) {
+    // todo
+}
+
+function update($key, $data) {
+    // todo
+}
+
+function delete($key) {
+    // todo
+}
